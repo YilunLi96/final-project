0cd8a7c3261728ca253f6b7f80c7dd828f1b29a7
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-06 18:04:34 -0500

    ahaahahaha

---
+++ setup.py
@@ -0,0 +1,15 @@
+requirements = ['math', 'theory', 'technical', 'breadth']
+
+class cs182:
+	name = ['cs182']
+	# concurrent?
+	pre_req = ['cs51', 'stat110']
+	satisfying_req = ['breadth', 'elective']
+	semester = ['fall']
+
+class cs181:
+	name = ['cs181']
+	pre_req = ['cs51', 'stat110', '']
+	satisfying_req = ['breadth', 'elective']
+	semester = ['fall']
+
