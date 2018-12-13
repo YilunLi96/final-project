5c2a0b3e651a520ffb60c039ee6e7bad2c8b2f09
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-13 17:52:12 -0500

    starting

---
+++ localsearch.py
@@ -0,0 +1,9 @@
+from readdata import *
+
+course_catalog = read_catalog()
+
+def hill_climbing():
+	
+	return
+
+# print len(course_catalog)
---
+++ main.py
@@ -0,0 +1,5 @@
+from localsearch import *
+
+def main():
+	return
+
