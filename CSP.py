51ac20d4d3ab49c6604d1303f6d09e66ea8869ae
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-15 15:35:52 -0500

    3 more functions then done

--- CSP.py
+++ CSP.py
@@ -8,29 +8,41 @@ def backtracking():
 	return recursive_backtracking()
 
 def recursive_backtracking(assignment):
+	# if assignment is complete return it
 	if course_list_complete(assignment):
 		return assignment
 
 	else:
+		# locate the next class slot to be filled
 		class_index = next_class_to_fill(assignment)
+		# get all the possible courses that can fulfill the next slot
 		possible_courses = valid_options(class_index)
-		
+
+		# go over them
+		for course in possible_courses:
+			# if it is already in assignment skip it
+			if course in assignment:
+				continue
+			if no_violations(assignment, course):
+				assignment = add_course(assignment, course)
+				result = recursive_backtracking(assignment)
+				if result != None:
+					return result
+				else:
+					assignment = remove_course(c_list, course)
+
 	return None
 
 
-# # returns true if there are no violations from duplicates or prereqs, otherwise false
-# # no need to check from requirements since we know the order they are satisfied in
-# def no_violations(c_list):
+# returns true if there are no violations from duplicates or prereqs, otherwise false
+# no need to check from requirements since we know the order they are satisfied in
+def no_violations(c_list, course):
+	return True
 
-# # returns a list of all valid successor course lists
-# def get_all_successors(c_list):
-#     # call next_class_to_fill to get next class_id
+# replace the first zero in c_list to course
+def add_course(c_list, course):
+	return c_list
 
-#     # call valid_options for a list of classes that satisfy the next req
-
-#     # create empty list to hold course lists that will be returned
-#     # for each class in this list
-#         # if inserting this class causes no conflicts append updated course list to list of successors
-#         # use no_violations to do this
-
-#     # return list of successor course lists
+# replace this course with zero
+def remove_course(c_list, course):
+	return c_list
