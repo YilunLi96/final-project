12d1ba499b985b8d55e5c52a78fd9c0a2664629e
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-15 00:17:38 -0500

    pseudo CSP

--- CSP.py
+++ CSP.py
@@ -11,5 +11,5 @@ reqs = ['linalg', 'calc', 'basic', 'boaz', 'theory', 'tech', 'breadth']
 # 2, start filling in courses following that above order
 	# 3, search in course catalog courses that satisfies this requirement 
 	# 4, randomly choose one course and test if there is violation, if not fill it in; if yes next random course
-	# 5, if the requirement is fulfilled, delete all courses that only satisfies this one requirement from course catalog ("filtering")
+	# 5, (maybe don't do it first, then add it, then compare speed? maybe too extra?)if the requirement is fulfilled, delete all courses that only satisfies this one requirement from course catalog ("filtering")
 # 6 repeat until full
