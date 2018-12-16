ff200293d4bd71aed60186834e69aaaa623ebce8
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-16 03:08:35 -0500

    statistics set up

--- algo_statistics.py
+++ algo_statistics.py
@@ -1,3 +1,24 @@
 from localsearch import *
 from CSP import *
+from course_setup import *
+from weighted_courses import *
+from have_to_take import *
 
+
+# 0. on the graph, title needs to include that this is for non-honor track
+# 1. run each of the following three () a bunch of iterations (1000? 10000?), then compare speed of each
+# 2. out of those iterations, for each funtion, draw the histogram of the most frequent courses
+iterative_less_conflicts()
+simulated_annealing()
+backtracking(CSP_initial)
+
+# 0. on the graph, title needs to include that this is for non-honor track
+# 0.5 title includes that in this example, the must-takes are: 182, 124, 61
+# 1. run each of the following three () a bunch of iterations (1000? 10000?), then compare speed of each
+# 2. out of those iterations, for each funtion, draw the histogram of the most frequent courses
+iterative_less_conflicts_have_to_take()
+backtracking(set_CSP_initial())
+
+# 1. run a bunch of iterations (1000? 10000?), then calculate speed
+# 2. simulated_annealing_weighted()[1] is the utility, calculate the expected utility out of those iterations
+simulated_annealing_weighted()
