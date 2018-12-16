508cd37feec9b3f2a6390d9763172c1968e096d8
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-16 02:58:01 -0500

    have to take CSP

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from weighted_courses import *
 from have_to_take import *
 
 
-print iterative_less_conflicts()
+# print iterative_less_conflicts()
 
-print simulated_annealing()
+# print simulated_annealing()
 
-print backtracking(CSP_initial)
+# print backtracking(CSP_initial)
 
-print simulated_annealing_weighted()
+# print simulated_annealing_weighted()
 
-print sort_class(iterative_less_conflicts_have_to_take())
+# print sort_class(iterative_less_conflicts_have_to_take())
+
+print backtracking(set_CSP_initial())
