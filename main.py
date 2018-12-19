629087667d440b1b7cbadbdb3e146d6da6a2a4a1
李以伦 <liyilun@liyilundeMacBook-Pro.local>
2018-12-18 20:54:53 -0500

    number

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ from weighted_courses import *
 from have_to_take import *
 
 
+# 1
 print iterative_less_conflicts()
 
+# 2
 print simulated_annealing()
 
+# 3
 print backtracking(CSP_initial)
 
-print simulated_annealing_weighted()
-
-print iterative_less_conflicts_have_to_take()
-
+# 4
 print backtracking(set_CSP_initial())
 
+# 5
+print iterative_less_conflicts_have_to_take()
+
+# 6
+print simulated_annealing_weighted()
+
+# 7
 print CSP_uniform_cost_weighted(CSP_initial2)
